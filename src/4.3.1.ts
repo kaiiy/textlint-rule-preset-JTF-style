49f06ca8eda211416ce2012566ef3df9c2f71d8d
kaiiy <81578246+kaiiy@users.noreply.github.com>
2023-07-03 00:33:00 +0900

    wip: 2023-07-03 00:33:00+09:00

--- src/4.3.1.ts
+++ src/4.3.1.ts
@@ -14,45 +14,48 @@ import type { TextlintRuleContext } from "@textlint/types";
 import type { TxtNode } from "@textlint/ast-node-types";
 
 const replaceSymbol = (symbol: string) => {
-    var newSymbol = {
-        "(": "（",
-        ")": "）"
-    }[symbol];
-    if (!newSymbol) {
-        throw new Error("fail to replace symbol");
-    }
-    return newSymbol;
+    if (symbol === "(") return "（";
+    else if (symbol === ")") return "）";
+    else throw new Error("fail to replace symbol");
 };
-function reporter(context: TextlintRuleContext) {
-    let { Syntax, RuleError, report, fixer, getSource } = context;
+
+const reporter = (context: TextlintRuleContext) => {
+    const { Syntax, RuleError, report, fixer, getSource } = context;
     return {
         [Syntax.Str](node: TxtNode) {
             if (!isUserWrittenNode(node, context)) {
                 return;
             }
             // 半角のかっこ()は使用しないで全角のかっこを使用する
-            const text = getSource(node);
-            const matchRegExps = [
+            const text: string = getSource(node);
+
+            // TODO: ()内に日本語を1文字でも含んでいる場合に変更
+            // (ascii) -> そのまま
+            // (日本語) -> キャプチャ
+            // (ascii+日本語) -> キャプチャ
+            // (）の半角全角混合パターンに注意
+            const matchRegExps: RegExp[] = [
                 // FIXME: https://github.com/textlint-ja/textlint-rule-preset-JTF-style/issues/79
                 // rx`([\(\)])(?:${japaneseRegExp}+)([\(\)])`,
                 // rx`([\(\)])(?:${japaneseRegExp})`,
                 rx`(?:${japaneseRegExp})([\(\)])`
             ];
-            matchRegExps.forEach((matchRegExp) => {
+            matchRegExps.forEach((matchRegExp: RegExp) => {
                 matchCaptureGroupAll(text, matchRegExp).forEach((match) => {
-                    const { index } = match;
+                    const { index, text } = match;
                     report(
                         node,
                         new RuleError("半角のかっこ()が使用されています。全角のかっこ（）を使用してください。", {
                             index: index,
-                            fix: fixer.replaceTextRange([index, index + 1], replaceSymbol(match.text))
+                            fix: fixer.replaceTextRange([index, index + 1], replaceSymbol(text))
                         })
                     );
                 });
             });
         }
     };
-}
+};
+
 module.exports = {
     linter: reporter,
     fixer: reporter
